9a0610002215c4561dca1c3b442a3b3a6cd7007b
rizankhan <rizankhan837@gmail.com>
2022-09-26 21:48:25 +0500

    Added Exercises

---
+++ Solidity Exercises/Searching Array.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: UNLICENSED 
+pragma solidity ^0.8.12;
+
+contract Search{
+	function search(uint[] memory array, uint len, uint element) public pure returns (uint){
+        uint num;
+        for (uint i = 0; i < len; i++){
+            if (element == array[i]){
+                num = 1;
+                break;
+            }else{
+                num = 0;
+            }
+        }
+        return num;
+    }
+}
