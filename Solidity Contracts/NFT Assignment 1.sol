ab83b47f2d5de47c0cc5edc6cc81bc97b0b725c9
rizankhan <rizankhan837@gmail.com>
2022-12-10 08:24:08 +0500

    Updated Lab 7 and 8

---
+++ Solidity Contracts/NFT Assignment 1.sol
@@ -0,0 +1,83 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/utils/cryptography/draft-EIP712.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/draft-ERC721Votes.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract NFT is ERC721, ERC721Enumerable, ERC721URIStorage, Pausable, Ownable, ERC721Burnable, EIP712, ERC721Votes {
+    using Counters for Counters.Counter;
+
+    Counters.Counter public _tokenIdCounter;
+    mapping(address => uint256) public mintCount;
+
+    constructor() ERC721("NFT", "NFT") EIP712("NFT", "1") {}
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "https://example.com/metadata";
+    }
+
+    function pause() private onlyOwner {
+        _pause();
+    }
+
+    function unpause() private onlyOwner {
+        _unpause();
+    }
+
+    function safeMint(address to) public onlyOwner payable{
+        require(msg.value >= 1000000000000000 wei, "Not Enough Balance");
+        payable(msg.sender).transfer(address(this).balance);
+        _tokenIdCounter.increment();
+        uint256 tokenId = _tokenIdCounter.current();
+        require(tokenId <= 500, "Can't Mint More Than 500 NFT's");
+        mintCount[msg.sender]++;
+        require(mintCount[msg.sender] <= 3, "Can't Mint More Than 3 NFT's");
+        _safeMint(to, tokenId);
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId, uint256 batchSize)
+        internal
+        whenNotPaused
+        override(ERC721, ERC721Enumerable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId, batchSize);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _afterTokenTransfer(address from, address to, uint256 tokenId, uint256 batchSize)
+        internal
+        override(ERC721, ERC721Votes)
+    {
+        super._afterTokenTransfer(from, to, tokenId, batchSize);
+    }
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
