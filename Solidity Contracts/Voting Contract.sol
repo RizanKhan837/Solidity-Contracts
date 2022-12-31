f5381c189ff38eb2bba5c95315cd38cc7f4a4422
rizankhan <rizankhan837@gmail.com>
2022-12-31 10:12:20 +0500

    Updated

--- Solidity Contracts/Voting Contract.sol
+++ Solidity Contracts/Voting Contract.sol
@@ -12,6 +12,7 @@ contract VotingApp{
         require(owner == msg.sender, "You're Not An Owner");
         _;
     }
+    // function to determine if there is any duplicate voter
     modifier checkDuplicates(string memory _voterName){
         for (uint i=0; i< voterName.length; i++){
             require(keccak256(abi.encodePacked(_voterName)) != keccak256(abi.encodePacked(voterName[i])), "You've Already Voted...");
@@ -27,6 +28,7 @@ contract VotingApp{
 
     Votes[] public totalVotes;
 
+    // function to vote for a party 
     function Vote(string memory _name, string memory _party, uint _age) public checkDuplicates(_name) returns(string memory){
         require(_age >= 18, "You Can't Vote...");
         require(keccak256(abi.encodePacked(_party)) == keccak256(abi.encodePacked("MQM")) || 
@@ -38,6 +40,7 @@ contract VotingApp{
         status[_party]++;
         return "Your Vote Has Been Submitted";
     }
+    // function to show the total votes
     function showStatus(string memory _party) public view onlyOwner returns(uint){
         return status[_party];
     }
