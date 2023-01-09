bf961db40e66fe285b7ce20865f7c71573b740ef
rizankhan <rizankhan837@gmail.com>
2023-01-09 19:36:49 +0500

    Added Stegify

--- Stegify2.py
+++ Stegify2.py
@@ -6,9 +6,9 @@ def decode_string_from_image(image):
         pixels = f.read()
 
     # Initialize variables for decoding
-    array_bit = 0
+    array_bit = []
     array_bit_index = 0
-    end_string = 0
+    end_string = []
     end_string_index = 0
     i = 0
     result = ""
@@ -16,9 +16,9 @@ def decode_string_from_image(image):
     # Decode the string from the pixel data
     while array_bit_index < 56:
         pixel_value = pixels[i]
-        array_bit = array_bit[array_bit_index]
+        array_bit.append(array_bit[array_bit_index])
         pixel_value += 0
-        if array_bit == 0:
+        if array_bit[array_bit_index] == 0:
             i += 4
             array_bit_index += 4
             i += 1
@@ -30,9 +30,9 @@ def decode_string_from_image(image):
             i += 1
     while end_string_index < 8:
         pixel_value = pixels[i]
-        end_string = end_string[end_string_index]
+        end_string.append(end_string[end_string_index])
         pixel_value += 0
-        if end_string == 0:
+        if end_string[end_string_index] == 0:
             i += 4
             end_string_index += 4
             i += 1
@@ -50,4 +50,4 @@ def decode_string_from_image(image):
     return result
 
 # Test the function
-print(decode_string_from_image("C:/Users/HP/OneDrive/Steganography_Project/ImageSource/sample.bmp"))
+print(decode_string_from_image("C:/Users/HP/OneDrive/Steganography_Project/decoded.bmp"))
