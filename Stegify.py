070e77f52f0f0923038ec1e70540330c239cd127
rizankhan <rizankhan837@gmail.com>
2023-01-09 19:07:03 +0500

    Applied Some Changing

--- Stegify.py
+++ Stegify.py
@@ -18,7 +18,7 @@ def decode_string_from_bitmap(bitmap_file, length_array_bits, array_bits, end_st
       t4 = file.read(1)
       # Read the value of the current array bit
       t6 = array_bits[j]
-      t4 += 0
+      t4 += bytes([0])
 
       # If the current array bit is 1, add 1 to the value of the pixel
       if t6 == 1:
@@ -61,7 +61,7 @@ def decode_string_from_bitmap(bitmap_file, length_array_bits, array_bits, end_st
       i += 1
       
   # Open the bitmap file again, this time in read-only mode
-  with open(bitmap_file, 'rb') as file:
+  with open(bitmap_file, 'wb') as file:
     # Seek to the first pixel address (56 bits after the start of the file)
     file.seek(56)
 
@@ -83,23 +83,53 @@ def decode_string_from_bitmap(bitmap_file, length_array_bits, array_bits, end_st
   return decoded_string
 
 # Calculate the length of the array of bits, the array of bits, and the end string from the given bitmap image
-with open('C:/Users/HP/OneDrive/Steganography_Project/ImageTextCacher/decoded.bmp', 'rb') as file:
+# with open('C:/Users/HP/OneDrive/Steganography_Project/ImageTextCacher/decoded.bmp', 'rb') as file:
+#   # Read the length of the array of bits from the first 4 bytes of the file
+#   length_array_bits = struct.unpack('>I', file.read(4))[0]
+#   # Initialize the array of bits
+#   array_bits = []
+#   # Read the array of bits from the next length_array_bits bytes of the file
+#   for i in range(length_array_bits):
+#     array_bits.append(struct.unpack('>B', file.read(1))[0])
+#   # Initialize the end string
+#   end_string = []
+#   # Read the end string from the next 8 bytes of the file
+#   for i in range(8):
+#     end_string.append(struct.unpack('>B', file.read(1))[0])
+
+# # Call the decode_string_from_bitmap function with the calculated arguments
+# decoded_string = decode_string_from_bitmap('C:/Users/HP/OneDrive/Steganography_Project/ImageTextCacher/decoded.bmp', length_array_bits, array_bits, end_string)
+
+# # Print the decoded string
+# print(decoded_string)
+
+# Calculate the length of the array of bits, the array of bits, and the end string from the given bitmap image
+with open('C:/Users/HP/OneDrive/Steganography_Project/decoded.bmp', 'rb') as file:
   # Read the length of the array of bits from the first 4 bytes of the file
   length_array_bits = struct.unpack('>I', file.read(4))[0]
   # Initialize the array of bits
   array_bits = []
-  # Read the array of bits from the next length_array_bits bytes of the file
+  
   for i in range(length_array_bits):
     array_bits.append(struct.unpack('>B', file.read(1))[0])
+  
   # Initialize the end string
   end_string = []
   # Read the end string from the next 8 bytes of the file
   for i in range(8):
-    end_string.append(struct.unpack('>B', file.read(1))[0])
+    # Read the next byte from the file
+    data = file.read(1)
+    # If the end of the file has been reached, break out of the loop
+    if not data:
+      break
+    # Unpack the byte and append it to the end string
+    end_string.append(struct.unpack('>B', data)[0])
 
+  
 # Call the decode_string_from_bitmap function with the calculated arguments
-decoded_string = decode_string_from_bitmap('C:/Users/HP/OneDrive/Steganography_Project/ImageTextCacher/decoded.bmp', length_array_bits, array_bits, end_string)
+decoded_string = decode_string_from_bitmap('C:/Users/HP/OneDrive/Steganography_Project/ImageSource/sample.bmp', length_array_bits, array_bits, end_string)
 
 # Print the decoded string
 print(decoded_string)
+
 
