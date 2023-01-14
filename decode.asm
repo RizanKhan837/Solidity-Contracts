fcd2512d8d57b899b29dc876ef7e32228b25228e
rizankhan <rizankhan837@gmail.com>
2023-01-14 23:11:00 +0500

    Updated

--- decode.asm
+++ decode.asm
@@ -50,7 +50,7 @@ syscall
 
 # Decode function
 decode:
-    # Loop through each pixel of the image
+    # Loop through each pixel of the
     loop:
         # Load the value of the current pixel into $t0
         lw $t0, 0($a0)
