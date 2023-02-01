af86e08c9317ffdf48165e38fdd7f264e4eae6c6
rizankhan <rizankhan837@gmail.com>
2023-02-01 23:21:01 +0500

    Updated

--- Solidity Exercises/Remainder.sol
+++ Solidity Exercises/Remainder.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED 
 pragma solidity ^0.8.12;
 
+// calculate the remainder of the division of two numbers
 contract Remainder{
     function find(uint _a) public pure returns(uint){
         require(_a > 0 , "num must be greater");
