5706dd9306f4e46eb9e82fcf44bedd1dfd0d1eda
rizankhan <rizankhan837@gmail.com>
2023-02-01 23:19:35 +0500

    Updated

--- Solidity Exercises/ExpressionSolving.sol
+++ Solidity Exercises/ExpressionSolving.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED 
 pragma solidity ^0.8.12;
 
+// calculate the sum of the first n terms of the expression x + x^2 + x^3 + ... + x^n
 contract Expression{
     function expression(uint x, uint n) public pure returns(uint){
         uint num = 1;
