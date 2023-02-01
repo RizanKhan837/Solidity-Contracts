560b8887f3d22a6a4f09d496dacf3f2e1e74944f
rizankhan <rizankhan837@gmail.com>
2023-02-01 23:19:50 +0500

    Updated

--- Solidity Exercises/HelloSolidity.sol
+++ Solidity Exercises/HelloSolidity.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED 
 pragma solidity ^0.8.12;
 
+// print the string "Hello Solidity"
 contract HelloSolidity{
     string str = "Hello Solidity";
     function print() public view returns(string memory){
