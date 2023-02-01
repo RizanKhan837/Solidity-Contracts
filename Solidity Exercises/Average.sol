0b3aaee8247c3440534920531fbc79931e7aa286
rizankhan <rizankhan837@gmail.com>
2023-02-01 23:19:03 +0500

    Updated

--- Solidity Exercises/Average.sol
+++ Solidity Exercises/Average.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED 
 pragma solidity ^0.8.12;
 
+// calculate the average of three numbers
 contract Average{
     function average(int a, int b, int c)public pure returns(int){
         int sum = a+b+c;
