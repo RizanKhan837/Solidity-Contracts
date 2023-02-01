7073b4ec998e665475227fae9d3b83b411fd3bb9
rizankhan <rizankhan837@gmail.com>
2023-02-01 23:22:17 +0500

    Updated

--- Solidity Exercises/SumOfArray.sol
+++ Solidity Exercises/SumOfArray.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED 
 pragma solidity ^0.8.12;
 
+
+// calculate the sum of all elements in an array
 contract SumOfArray{
     function sumOfArray(uint[] memory arrays, uint len) public pure returns(uint){
         uint num = 0;
