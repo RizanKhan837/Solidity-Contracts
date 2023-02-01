62f4d8f3e2e10b72c8aff58f3fe644daf562239b
rizankhan <rizankhan837@gmail.com>
2023-02-01 23:22:06 +0500

    Updated

--- Solidity Exercises/SetterGetter.sol
+++ Solidity Exercises/SetterGetter.sol
@@ -1,5 +1,7 @@
 pragma solidity ^0.8.12;
 
+
+// create a contract that has a private variable and a function to set the value of the variable and a function to get the value of the variable
 contract SetAndGet{
     uint private num;
     function set(uint _num) public{
